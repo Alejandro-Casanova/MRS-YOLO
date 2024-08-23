8a8be204f4e3dd6da4c7d0ae79fefa39457c13fb
Alejandro Casanova <alejandro.casanova@disroot.org>
2024-08-23 20:27:05 +0900

    started training

--- eval.py
+++ eval.py
@@ -2,10 +2,11 @@ from ultralytics import YOLO
 from ultralytics.utils import yaml_load
 from ultralytics.utils.checks import check_yaml
 
+# model = YOLO("./best/bestC3.pt")  # Load a pretrained model
+# results = model.val(data="cfg/datasets/data.yaml", imgsz=640)
+# print(results.box.map)
+
 # Load Config
 cfg = yaml_load(check_yaml("cfg/my_config.yaml"))
-
-# Load a pretrained model
-model = YOLO('yolov8n.pt')  
-
+model = YOLO('yolov8n.pt')  # Load a pretrained model
 results = model.train(**cfg)
